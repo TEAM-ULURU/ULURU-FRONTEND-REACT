e75b95fe6c62642746a297ddcc313ad9831fdcbe
강찬욱 <114604267+chanwookK@users.noreply.github.com>
2024-08-06 05:22:56 +0900

    Update OAuthCallback.js

--- src/pages/OAuthCallback.js
+++ src/pages/OAuthCallback.js
@@ -14,7 +14,7 @@ const OAuthCallback = () => {
       if (state && code) {
         try {
           const response = await fetch(
-            `https://alt-backend.com/login/oauth2/code/google?state=${state}&code=${code}`,
+            `https://alt-back.run.goorm.io/login/oauth2/code/google?state=${state}&code=${code}`,
             {
               credentials: "include",
             }
