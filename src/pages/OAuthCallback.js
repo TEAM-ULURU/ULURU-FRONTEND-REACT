064a2a5f2bb1c5bc215995591f1d147c28694ed6
강찬욱 <114604267+chanwookK@users.noreply.github.com>
2024-08-06 02:45:32 +0900

    Update OAuthCallback.js

--- src/pages/OAuthCallback.js
+++ src/pages/OAuthCallback.js
@@ -14,7 +14,7 @@ const OAuthCallback = () => {
       if (state && code) {
         try {
           const response = await fetch(
-            `http://localhost:8080/login/oauth2/code/google?state=${state}&code=${code}`,
+            `http://ec2-18-116-81-21.us-east-2.compute.amazonaws.com:8080/login/oauth2/code/google?state=${state}&code=${code}`,
             {
               credentials: "include",
             }
